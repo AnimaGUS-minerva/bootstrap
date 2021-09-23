8286a9075fa87c310540f2aae0b6d0e5ec5d1869
Michael Richardson <mcr@sandelman.ca>
2021-09-22 23:44:38 -0400

    added basic set of command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use std::sync::Arc;
+use std::path::PathBuf;
 use tokio::runtime;
 use structopt::StructOpt;
+use url::Url;
 
 static VERSION: &str = "0.9.0";
+static DEFAULT_JOIN_THREADS: u16 = 16;
 
 #[derive(StructOpt)]
 // Hermes Connect Autonomic Control Plane (ACP) manager
@@ -10,7 +13,43 @@ struct ConnectOptions {
     // turn on debugging from Grasp DULL
     #[structopt(default_value = "false", long, parse(try_from_str))]
     debug_bootstrap: bool,
+
+    // override search and just connect to Registrar URI provided
+    #[structopt(parse(try_from_str = Url::parse))]
+    registrar: Option<Url>,
+
+    // where to find the IDevID certificate
+    #[structopt(parse(from_os_str))]
+    idevid_cert: Option<PathBuf>,
+
+    // where to find the IDevID private key
+    #[structopt(parse(from_os_str))]
+    idevid_priv: Option<PathBuf>,
+
+    /// Output dir for LDevID
+    #[structopt(parse(from_os_str))]
+    ldevid_cert: Option<PathBuf>,
 }
+
+/*
+ * Bootstrap is a program in a few distinct states.
+ *
+ * 1. It has no IDevID provisioned as yet. It waits for one to show up,
+ *    so that it can move to state 2.
+ *
+ * 2. It has an IDevID private key and certificate, so it looks for
+ *    candidate Join Proxy on each of it's physical interfaces.
+ *
+ * 3. For each physical interface found, a thread is created to start an mbedtls
+ *    connection to the join proxy, to start onboarding via BRSKI (RFC8995).
+ *    A maximum of ConnectionOptions.join_threads is allowed to run.
+ *    Any additional ones are put in a queue.
+ *
+ * 4. If a thread takes too long, or fails, then the interface is put back on
+ *    the queue to be dealt with soon.
+ *
+*/
+
 
 async fn bootstrap(_args: ConnectOptions) -> Result<(), String> {
     println!("Hello");
@@ -39,6 +78,8 @@ fn main () -> Result<(), String> {
 
     return Ok(());
 }
+
+
 
 /*
  * Local Variables:
